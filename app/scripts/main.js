516b1846b5c8abf73bf5b00ce4b7ffb2960b4da4
darrick fabian <dfabian@solutionset.com>
2015-11-11 22:38:06 -0800

    wip iframe ref

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,14 +5,25 @@ $( document ).ready(function() {
       url: "http://reddit.com/r/listentothis/new/.json?jsonp=callbackFunction",
       dataType: 'jsonp',
       jsonpCallback : 'callbackFunction',
+      data: JSON.stringify(),
       success: function(response) {
           console.log( response ); // server response
           $('#holdIt').append('word');
 
         //   console.log(response.data.children);
+        var items = [];
           $.each(response.data.children, function(index, el) {
               console.log("index ", index);
               console.log("element ", el.data.author);
+              console.log("media ", el.data.media_embed.content);
+              // items.push("<div>" + el.data.media_embed.content + "</div>");
+
+              $('#results').append(
+                  "<pre>"
+                   + el.data.media_embed.content +
+                  "</pre>"
+              );
+
           });
 
       },
