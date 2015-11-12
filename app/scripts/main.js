ba3f28d9960ace097b979ae9f90a6b6f363352fd
darrickfabian <dfabian@epsilon.com>
2015-11-11 18:43:38 -0800

    funsies log

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,4 @@
-console.log('\'Allo \'Allo!');
 $( document ).ready(function() {
-  // $.ajax({
-  //     url: "http://query.yahooapis.com/v1/public/yql",
-  //
-  //     // The name of the callback parameter, as specified by the YQL service
-  //     jsonp: "callback",
-  //
-  //     // Tell jQuery we're expecting JSONP
-  //     dataType: "jsonp",
-  //
-  //     // Tell YQL what we want and that we want JSON
-  //     data: {
-  //         q: "select title,abstract,url from search.news where query=\"cat\"",
-  //         format: "json"
-  //     },
-  //
-  //     // Work with the response
-  //     success: function( response ) {
-  //         console.log( response ); // server response
-  //     }
-  // });
-
   //https://www.reddit.com/r/learnprogramming/comments/2y3ljg/error_when_calling_the_reddit_api_for_jsonp_return/
 
   $.ajax({
@@ -29,10 +7,22 @@ $( document ).ready(function() {
       jsonpCallback : 'callbackFunction',
       success: function(response) {
           console.log( response ); // server response
+          $('#holdIt').append('word');
+
+        //   console.log(response.data.children);
+          $.each(response.data.children, function(index, el) {
+              console.log("index ", index);
+              console.log("element ", el.data.author);
+          });
+
       },
       error: function(xhr, status, error) {
           alert("fail");
       }
   });
+
+
+
+
 
 });
