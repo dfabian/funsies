700dea6371a0fc60200ff695fa66174df599b9b6
darrickfabian <dfabian@epsilon.com>
2015-11-12 16:29:36 -0800

    update the fun

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,6 @@ $( document ).ready(function() {
       url: "http://reddit.com/r/listentothis/new/.json?jsonp=callbackFunction",
       dataType: 'jsonp',
       jsonpCallback : 'callbackFunction',
-      data: JSON.stringify(),
       success: function(response) {
           console.log( response ); // server response
           $('#holdIt').append('word');
@@ -13,16 +12,17 @@ $( document ).ready(function() {
         //   console.log(response.data.children);
         var items = [];
           $.each(response.data.children, function(index, el) {
-              console.log("index ", index);
-              console.log("element ", el.data.author);
-              console.log("media ", el.data.media_embed.content);
-              // items.push("<div>" + el.data.media_embed.content + "</div>");
+              var iFrame = $('<textarea />').html(el.data.media_embed.content).text();
+              if (el.data.created !== null) {
+                  console.log("index ", index);
+                  console.log("element ", el.data.author);
+                  console.log("media ", $('<textarea />').html(el.data.media_embed.content));
+                  // items.push("<div>" + el.data.media_embed.content + "</div>");
+                  // look at timestamp value -- if the item is less than a week old attach a 'new' class to the element with new css style, if the item is less than a month old, attach a 'somewhat-new' css class, otherwise, give it a default style... page 202, loop throuh the nodes and look for the ones with the 'new' class... add flag icon through looping. display: author, link_flair, and image...
 
-              $('#results').append(
-                  "<pre>"
-                   + el.data.media_embed.content +
-                  "</pre>"
-              );
+                  //read up on date object, mdn
+                  $('#results').append(iFrame);
+              };
 
           });
 
