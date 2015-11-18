edc6c45fd3addd9475998ca7db0ba3800217f655
darrick fabian <dfabian@solutionset.com>
2015-11-18 09:18:26 -0800

    wip

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,15 +13,26 @@ $( document ).ready(function() {
         var items = [];
           $.each(response.data.children, function(index, el) {
               var iFrame = $('<textarea />').html(el.data.media_embed.content).text();
-              if (el.data.created !== null) {
+              //var thumb = el.data.media.oembed.thumbnail_url;
+              var createdDate = new Date(el.data.created * 1000);
+              var createdDate = el.data.created;
+              if (createdDate <= createdDate - 60000) {
                   console.log("index ", index);
                   console.log("element ", el.data.author);
-                  console.log("media ", $('<textarea />').html(el.data.media_embed.content));
+                  console.log("time ", createdDate);
+                  //console.log("thumb url ", thumb);
+                  //console.log("media ", $('<textarea />').html(el.data.media_embed.content));
                   // items.push("<div>" + el.data.media_embed.content + "</div>");
                   // look at timestamp value -- if the item is less than a week old attach a 'new' class to the element with new css style, if the item is less than a month old, attach a 'somewhat-new' css class, otherwise, give it a default style... page 202, loop throuh the nodes and look for the ones with the 'new' class... add flag icon through looping. display: author, link_flair, and image...
 
                   //read up on date object, mdn
                   $('#results').append(iFrame);
+              } else if (createdDate <= createdDate - 3600000) {
+                console.log("more than an hour in the past ", createdDate);
+              } else if(createdDate <= createdDate - 3600001){
+                console.log("more than two hours in the past ", createdDate);
+              } else {
+                console.log("meh");
               };
 
           });
