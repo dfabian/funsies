56bfb388770bb2932bd69887459b0b6b512a2bc8
darrick fabian <dfabian@solutionset.com>
2015-11-19 23:45:23 -0800

    wip

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,32 +7,46 @@ $( document ).ready(function() {
       jsonpCallback : 'callbackFunction',
       success: function(response) {
           console.log( response ); // server response
-          $('#holdIt').append('word');
-
-        //   console.log(response.data.children);
+          //http://stackoverflow.com/questions/32857311/how-to-check-and-set-if-current-time-is-out-of-specific-range-javascript-jquery?rq=1
         var items = [];
           $.each(response.data.children, function(index, el) {
-              var iFrame = $('<textarea />').html(el.data.media_embed.content).text();
-              //var thumb = el.data.media.oembed.thumbnail_url;
+              var title = $('<div />').append(el.data.title);
+              var date = new Date;
+              var currentHour = date.getUTCHours();
               var createdDate = new Date(el.data.created * 1000);
-              var createdDate = el.data.created;
-              if (createdDate <= createdDate - 60000) {
-                  console.log("index ", index);
-                  console.log("element ", el.data.author);
+              var hourPosted = createdDate.getUTCHours();
+              if (hourPosted < currentHour + 1) {
+                  // console.log("index ", index);
+                  // console.log("element ", el.data.author);
                   console.log("time ", createdDate);
-                  //console.log("thumb url ", thumb);
-                  //console.log("media ", $('<textarea />').html(el.data.media_embed.content));
-                  // items.push("<div>" + el.data.media_embed.content + "</div>");
-                  // look at timestamp value -- if the item is less than a week old attach a 'new' class to the element with new css style, if the item is less than a month old, attach a 'somewhat-new' css class, otherwise, give it a default style... page 202, loop throuh the nodes and look for the ones with the 'new' class... add flag icon through looping. display: author, link_flair, and image...
+                  console.log("hour ", hourPosted);
+                  console.log("current ", currentHour);
 
+                  // console.log("less than an hour in the past ", createdDate);
+
+                  // look at timestamp value -- if the item is less than a week old attach a 'new' class to the element with new css style, if the item is less than a month old, attach a 'somewhat-new' css class, otherwise, give it a default style... page 202, loop throuh the nodes and look for the ones with the 'new' class... add flag icon through looping. display: author, link_flair, and image...
                   //read up on date object, mdn
-                  $('#results').append(iFrame);
-              } else if (createdDate <= createdDate - 3600000) {
+                  // $('<div></div>').addClass('test2').insertAfter('#results').text(title);
+                  $('#results').append(title, createdDate).addClass('test');
+
+              } else if (hourPosted > currentHour + 2) {
+                $('#results').append(title, createdDate);
                 console.log("more than an hour in the past ", createdDate);
-              } else if(createdDate <= createdDate - 3600001){
+                console.log("time ", createdDate);
+                console.log("hour ", hourPosted);
+                console.log("current ", currentHour);
+              } else if(hourPosted > currentHour + 6){
+                $('#results').append(title, createdDate);
                 console.log("more than two hours in the past ", createdDate);
+                console.log("time ", createdDate);
+                console.log("hour ", hourPosted);
+                console.log("current ", currentHour);
               } else {
-                console.log("meh");
+                $('#results').append(title, createdDate);
+                console.log("more than three hours in the past ", createdDate);
+                console.log("time ", createdDate);
+                console.log("hour ", hourPosted);
+                console.log("current ", currentHour);
               };
 
           });
